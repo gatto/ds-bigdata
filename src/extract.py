9693562eeadd3a44420ac4f4b3de9b2685dcf6d2
Fabio <4898010+gatto@users.noreply.github.com>
2021-10-13 11:48:34 +0200

    integrate Saverio's code

--- src/extract.py
+++ src/extract.py
@@ -4,13 +4,17 @@ from pathlib import Path
 import attr
 import pandas as pd
 
-DATA_PATH = pkg_resources.resource_filename("extractbda", "data/")
+try:
+    DATA_PATH = pkg_resources.resource_filename("extractbda", "data/")
+except ModuleNotFoundError:
+    DATA_PATH = "data/"
 
 
 @attr.s
 class Bikes:
     df = attr.ib()
-    df_ds = attr.ib()
+    df_date = attr.ib()
+    df_full = attr.ib()
 
     @df.default
     def _df_default(self):
@@ -22,12 +26,18 @@ class Bikes:
         my_df = my_df.drop(cols_to_drop, axis=1)
         return my_df
 
-    @df_ds.default
-    def _df_ds_default(self):
-        # options
+    @df_date.default
+    def _df_date_default(self):
         my_df = self.df
-        # my_df["datestamp"] = list(map(lambda x, y: x.replace(hour=y), my_df["dteday"], my_df["hr"]))
+        my_df["dteday"] = pd.to_datetime(my_df["dteday"])
+        my_df["datestamp"] = list(
+            map(lambda x, y: x.replace(hour=y), my_df["dteday"], my_df["hr"])
+        )
         return my_df
+
+    @df_full.default
+    def _df_full_default(self):
+        return self._fill_timestamps(self.df_date)
 
     def _load(self, file):
         my_csv = Path(f"{DATA_PATH}{file}.csv")
@@ -37,3 +47,22 @@ class Bikes:
         except FileNotFoundError:
             pipi = pd.read_csv(my_csv)
         return pipi
+
+    def _fill_timestamps(self, df):
+        """
+        input dataframe must have the datestamp
+        create pd.Series with all timestamps from 2011-01-01 00:00:00 to 2012-12-31 23:00:00
+        """
+        idx = pd.Series(
+            pd.date_range(
+                "2011-01-01 00:00:00",
+                "2012-12-31 23:00:00",
+                freq="H",
+            ),
+            name="datestamp_full",
+        )
+        # join new pd.Series with our dataset and delete old datestamp
+        result = pd.merge(
+            df, idx, left_on="datestamp", right_on="datestamp_full", how="right"
+        ).drop("datestamp", axis=1)
+        return result
